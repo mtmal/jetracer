5974fb1bf7fcea69de51099a503f81338a72cb9f
mtmal <mt@mal>
2022-07-05 17:30:18 +0100

    Updated setup to explicitly list packages which work with Python 3.6 - default in jetpack 4.5.1.

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     description='An easy to use AI racecar powered by NVIDIA Jetson Nano',
     packages=find_packages(),
     install_requires=[
-        'adafruit-circuitpython-servokit'
+	'Adafruit-PlatformDetect==3.13.0',
+	'Adafruit-Blinka==6.10.3',
+	'adafruit-circuitpython-busdevice==5.0.6',
+	'adafruit-circuitpython-motor==3.3.0',
+        'adafruit-circuitpython-servokit==1.2.2'
     ],
 )
